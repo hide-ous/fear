e011068169fe396bdada0fcee443c8937d2c450f
hide-ous <mattia.samory@gmail.com>
2021-06-14 16:48:40 +0200

    * removed subreddits with low shared audience

--- subreddit_cooccurrence.py
+++ subreddit_cooccurrence.py
@@ -7,6 +7,8 @@ from collections import Counter, defaultdict
 from itertools import combinations
 
 import scipy as sp
+from scipy import sparse
+from tqdm import tqdm
 
 from utils import read_config, read_subreddit_lexicon
 
@@ -34,7 +36,9 @@ def find_users_in_seed_subreddits(author_subreddit_count):
             seed_subreddits.extend(subreddits)
     seed_subreddits = set(seed_subreddits)
     authors_in_seed_subreddits = set()
-    for author, subreddit_counts in author_subreddit_count.items():
+    for author, subreddit_counts in tqdm(author_subreddit_count.items(),
+                                         'filter authors in seed subreddits', len(author_subreddit_count)
+                                         ):
         subreddit_counts = set(subreddit_counts.keys())
         if len(seed_subreddits.intersection(subreddit_counts)) > 0:
             authors_in_seed_subreddits.add(author)
@@ -49,32 +53,45 @@ def read_author_subreddit_count():
     return author_subreddit_counts
 
 
-if __name__ == '__main__':
-
-    author_subreddit_count = read_author_subreddit_count()
+def subreddit_count_matrix(author_subreddit_count, min_subreddits_w_shared_audience=10):
+    # compute shared audience
     authors_in_seed_subreddits = find_users_in_seed_subreddits(author_subreddit_count)
-
     subreddit_author_count = Counter()
-    subreddits = set()
-    for author, subreddit_counts in author_subreddit_count.items():
+    for author, subreddit_counts in tqdm(author_subreddit_count.items(),
+                                         "compute shared audience",
+                                         len(author_subreddit_count)):
         if author in authors_in_seed_subreddits:  # limit to authors in the subreddits of interest
             for subreddit1, subreddit2 in combinations(subreddit_counts.keys(), 2):
                 subreddit_author_count[(subreddit1, subreddit2)] += 1
                 subreddit_author_count[(subreddit2, subreddit1)] += 1
-                subreddits.add(subreddit1)
-                subreddits.add(subreddit2)
 
+    # filter subreddits with enough shared audience
+    subreddits = Counter()
+    for subreddit1, subreddit2 in subreddit_author_count.keys():
+        if subreddit1 < subreddit2:  # avoid double counting
+            subreddit1 += 1  # number of subreddits with shared audience
+    subreddits = {subreddit for subreddit, count in subreddits.items() if count >= min_subreddits_w_shared_audience}
+    subreddit_author_count = {(subreddit1, subreddit2): count for (subreddit1, subreddit2), count in
+                              subreddit_author_count if (subreddit1 in subreddits) and (subreddit2 in subreddits)}
     subreddit_indices = list(sorted(subreddits))
 
+    # transform into sparse matrix
     rows = list()
     columns = list()
     values = list()
-
-    for (subreddit1, subreddit2), count in subreddit_author_count.items():
+    for (subreddit1, subreddit2), count in tqdm(subreddit_author_count.items(),
+                                                'transform into sparse matrix', len(subreddit_author_count)
+                                                ):
         rows.append(subreddit_indices.index(subreddit1))
         columns.append(subreddit_indices.index(subreddit2))
         values.append(count)
     counts = sp.sparse.csr_matrix((values, (rows, columns)))
+    return counts, subreddit_indices
+
+
+if __name__ == '__main__':
+    author_subreddit_count = read_author_subreddit_count()
+    counts, subreddit_indices = subreddit_count_matrix(author_subreddit_count)
 
     config = read_config()
     with open(os.path.join(config['data_root'], 'subreddit_cooccurrence.npy'), 'w+') as f:
--- subreddit_pmi.py
+++ subreddit_pmi.py
@@ -1,58 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
-import itertools
 import os
 import pickle
-from collections import Counter
-import random
 import numpy as np
 from tqdm import tqdm
 from scipy import sparse
 from scipy.sparse import linalg
 from sklearn.metrics.pairwise import cosine_similarity
 
-# def get_skipgrams(corpus, max_window, seed=42):
-#     rnd = random.Random()
-#     rnd.seed(a=seed)
-#     skipgrams = Counter()
-#     for doc in tqdm(
-#             corpus, total=len(corpus), desc='calculating skipgrams'
-#     ):
-#
-#         num_tokens = len(doc)
-#         if num_tokens == 1:
-#             continue
-#         for ii_word, word in enumerate(doc):
-#
-#             window = rnd.randint(1, max_window)
-#             ii_context_min = max(0, ii_word - window)
-#             ii_context_max = min(num_tokens - 1, ii_word + window)
-#             ii_contexts = [
-#                 ii for ii in range(ii_context_min, ii_context_max + 1)
-#                 if ii != ii_word]
-#             for ii_context in ii_contexts:
-#                 context = doc[ii_context]
-#                 skipgram = (word, context)
-#                 skipgrams[skipgram] += 1
-#
-#     return skipgrams
-
-
-# def get_count_matrix(skipgrams):
-#     row_indxs = []
-#     col_indxs = []
-#     dat_values = []
-#     for skipgram in tqdm(
-#             skipgrams.items(),
-#             total=len(skipgrams),
-#             desc='building count matrix row,col,dat'
-#     ):
-#         (tok_word_indx, tok_context_indx), sg_count = skipgram
-#         row_indxs.append(tok_word_indx)
-#         col_indxs.append(tok_context_indx)
-#         dat_values.append(sg_count)
-#     print('building sparse count matrix')
-#     return sparse.csr_matrix((dat_values, (row_indxs, col_indxs)))
 from utils import read_config
 
 
@@ -91,12 +46,10 @@ def get_ppmi_matrix(count_matrix, alpha=0.75):
         (tok_word_indx, tok_context_indx) = idxs
         pound_wc = count_matrix[tok_word_indx, tok_context_indx]
         pound_w = sum_over_contexts[tok_word_indx]
-        # pound_c = sum_over_words[tok_context_indx]
         pound_c_alpha = sum_over_words_alpha[tok_context_indx]
 
         Pwc = pound_wc / DD
         Pw = pound_w / DD
-        # Pc = pound_c / DD
         Pc_alpha = pound_c_alpha / Pc_alpha_denom
 
         pmi = np.log2(Pwc / (Pw * Pc_alpha))
@@ -112,13 +65,6 @@ def get_ppmi_matrix(count_matrix, alpha=0.75):
 
 def svd_ppmi(count_matrix, embedding_size=200, content_distribution_smoothing=0.75,
              svd_diag_exponent=0.5):
-    # skipgrams = get_skipgrams(corpus, window_length)
-
-    # count_matrix = get_count_matrix(skipgrams)
-
-    # # normalize rows
-    # count_matrix_l2 = normalize(count_matrix, norm='l2', axis=1)
-
     ppmi_matrix = get_ppmi_matrix(count_matrix, content_distribution_smoothing)
 
     uu, ss, vv = linalg.svds(ppmi_matrix, embedding_size)
@@ -134,22 +80,6 @@ def svd_ppmi(count_matrix, embedding_size=200, content_distribution_smoothing=0.
     return svd_word_vecs
 
 
-# def get_word_embeddings(docs, min_tok_freq=10, window_length=5, embedding_size=200, content_distribution_smoothing=0.75,
-#                         svd_diag_exponent=0.5):
-#     print("creating auxiliary data structures")
-#     flatten = lambda a: itertools.chain.from_iterable(a)
-#     unigram_counts = Counter(flatten(docs))
-#
-#     unigram_counts = dict(filter(lambda x: x[1] > min_tok_freq, unigram_counts.items()))
-#     id2tok = dict(enumerate(unigram_counts.keys()))
-#     tok2id = {i: j for j, i in id2tok.items()}
-#     corpus = [[tok2id[tok] for tok in doc if tok in tok2id] for doc in docs]
-#     print('computing ppmi')
-#     pmis = svd_ppmi(corpus, window_length=window_length, embedding_size=embedding_size,
-#                     content_distribution_smoothing=content_distribution_smoothing, svd_diag_exponent=svd_diag_exponent)
-#     return unigram_counts, id2tok, tok2id, pmis
-
-
 if __name__ == '__main__':
     config = read_config()
     with open(os.path.join(config['data_root'], 'subreddit_cooccurrence.npy'), 'r') as f:
@@ -160,15 +90,17 @@ if __name__ == '__main__':
     id2tok = {i: subreddit for i, subreddit in enumerate(subreddit_indices)}
     tok2id = {i: j for j, i in id2tok.items()}
     print('computing ppmi')
-    min_tok_freq = 10
-    window_length = 5
     embedding_size = 200
     content_distribution_smoothing = 0.75
-    svd_diag_exponent=0.5
+    svd_diag_exponent = 0.5
 
-    pmis = svd_ppmi(counts, window_length=window_length, embedding_size=embedding_size,
+    pmis = svd_ppmi(counts, embedding_size=embedding_size,
                     content_distribution_smoothing=content_distribution_smoothing, svd_diag_exponent=svd_diag_exponent)
 
+    with open(os.path.join(config['resources_root'], 'subreddit_pmi.npy'), 'w+') as f:
+        pickle.dump(pmis, f)
+    with open(os.path.join(config['resources_root'], 'subreddit_pmi.idx'), 'w+') as f:
+        pickle.dump(subreddit_indices, f)
 
     print(ww_sim('/r/conspiracy', pmis, tok2id, id2tok))
     print(ww_sim('/r/science', pmis, tok2id, id2tok))
